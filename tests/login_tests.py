8f3927c4a1c434b8e78661ac658749d5d6e53e73
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2014-07-22 17:32:21 +0300

    Test failure messages for login tests

--- tests/login_tests.py
+++ tests/login_tests.py
@@ -8,38 +8,42 @@ from endpoints import *
 class LoginTestCase(IgorApiTestCase):
 
     def test_root(self):
-        # / should be accessible without logging in
-        self.assert_200(self.client.get(url_for('root')))
+        self.assert_200(self.client.get(url_for('root')),
+                        message='root must be accessible without login')
 
     def test_login(self):
 
-        # No credentials provided
-        self.assert_401(self.client.get(url_for('login')))
+        self.assert_401(self.client.get(url_for('login')),
+                        message='login should fail without credentials')
 
-        # Incorrect username
         self.assert_401(self.client.get(url_for('login'),
                         headers=[('Authorization', 'Basic '
-                                                  + base64.b64encode('toor:root'))]))
+                                                  + base64.b64encode('toor:root'))]),
+                        message='login should fail with incorrect username')
 
-        # Incorrect password
         self.assert_401(self.client.get(url_for('login'),
                         headers=[('Authorization', 'Basic '
-                                                  + base64.b64encode('root:toor'))]))
+                                                  + base64.b64encode('root:toor'))]),
+                        message='login should fail with incorrect password')
 
-        # Incorrect username and password
         self.assert_401(self.client.get(url_for('login'),
                         headers=[('Authorization', 'Basic '
-                                                  + base64.b64encode('toor:toor'))]))
+                                                  + base64.b64encode('toor:toor'))]),
+                        message='login should fail with incorrect username and password')
         
-        # Correct username and password
         self.assert_200(self.client.get(url_for('login'),
                         headers=[('Authorization', 'Basic '
-                                                  + base64.b64encode('root:root'))]))
+                                                  + base64.b64encode('root:root'))]),
+                        message='login should pass with correct username and password')
 
     def test_unauthenticated_endpoints(self):
-        # Should not be able to access any endpoints without authentication
         for endpoint in all_endpoints:
-            self.assert_401(self.client.get(url_for(endpoint,
-                                                    username='username',
-                                                    hostname='hostname',
-                                                    channel=0)))
+
+            endpoint_url = url_for(endpoint,
+                                   username='username',
+                                   hostname='hostname',
+                                   channel=0)
+
+            self.assert_401(self.client.get(endpoint_url),
+                            message=endpoint_url +
+                            ' should be inaccessible without login')
